1848ed3f36b7fb43f855b622b3dafb23d78d5b39
military1 <mr.gusliakov@mail.ru>
2019-12-27 19:51:31 +0200

    Update settings.py

--- auth/settings.py
+++ auth/settings.py
@@ -10,7 +10,7 @@ VERSION = '3.0'
 SQLITE_DB = 'db/auth.db' 
 
 # maximum number of authorized IPs per user 
-MAX_AUTH_IP_COUNT = 255
+MAX_AUTH_IP_COUNT = 5
 
 # automatic IP authorization without authentication
 # do not set this to True, unless you *really* know what you are doing
